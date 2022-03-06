8bada17be9ed2db4040e0fab6def97b41bda91ce
Zi-Gao <gaogaohahaa@gmail.com>
2022-03-06 14:17:03 +0800

    .

--- Documents/Program/2022Spring/2022-3-5/2.cpp
+++ Documents/Program/2022Spring/2022-3-5/2.cpp
@@ -1,12 +1,7 @@
 #include<cstdio>
 #include<algorithm>
+#include<cstring>
 /*
-4 15
-100 4
-412 8
-266 7
-591 2
-1193.0
 https://vjudge.net/problem/ZOJ-1012
 10
 4 256 3
@@ -17,15 +12,22 @@ https://vjudge.net/problem/ZOJ-1012
 using namespace std;
 struct work{
 	int A,B,T,U,V,W,X,f;//A:CPU B:RAM T:start U:finish V:money W:+monety/h X:-money/h
+	int finish;
 	bool operator < (const work & a) const
 	{
+		if(finish){
+			return false;
+		}
+		if(a.finish){
+			return true;
+		}
 		if(T!=a.T){
 			return T<a.T;
 		}
 		return V>a.V;
 	}
-}works[1000];
-int C,R,N,F;
+};
+
 inline int read(){
     int x=0,f=1;char c=getchar();
     while(c<'0'||c>'9')c=='-'?f=-1:f,c=getchar();//?=if,:=else
@@ -36,32 +38,65 @@ inline int read(){
     return x*f;
 }
 int main(){
-	F=read();
-	C=read();
-	R=read();
-	N=read();
-	
-	for(int i=0;i<N;i++){
-		works[i].A=read();
-		works[i].B=read();
-		works[i].T=read();
-		works[i].U=read();
-		works[i].V=read();
-		works[i].W=read();
-		works[i].X=read();
-	}
-	
-	sort(works,works+N);
-	
-	putchar('\n');
-	
-	for(int i=0;i<N;i++){
-		printf("A:%d B:%d T:%d U:%d V:%d W:%d X:%d\n",works[i].A,works[i].B,works[i].T,works[i].U,works[i].V,works[i].W,works[i].X);
-	}
-	
-	for(int t=0;t<=F;t++){
-		for(int i=0;i<N;i++){
-			if()
+	for(int i=1;true;++i){
+		int C,R,N,F,money;
+		work works[1000];
+		memset(works,0,sizeof(work));
+		F=read();
+		if(F==0){
+			break;
 		}
+		C=read();
+		R=read();
+		N=read();
+
+		for(int i=0;i<N;i++){
+			works[i].A=read();
+			works[i].B=read();
+			works[i].T=read();
+			works[i].U=read();
+			works[i].V=read();
+			works[i].W=read();
+			works[i].X=read();
+			works[i].finish=-1;
+		}
+
+		sort(works,works+N);
+
+		for(int t=0;t<=F;t++){
+			for(int i=0;i<N;i++){
+				if(works[i].finish==t){
+					//释放资源
+					C+=works[i].A;
+					R+=works[i].B;
+				}
+				if(works[i].finish<t){
+					continue;
+				}
+				if(works[i].T>t){
+					continue;
+				}
+				if(works[i].A<=C&&works[i].B<=R){
+					//占用资源
+					C-=works[i].A;
+					R-=works[i].B;
+					works[i].finish=t+1;
+					//计算费用
+					if(works[i].finish<=works[i].U){
+						money+=works[i].V+works[i].W*(works[i].U-works[i].finish);
+					}else{
+						money+=works[i].V-works[i].X*(works[i].finish-works[i].U);
+					}
+				}
+				sort(works,works+N);
+			}
+		}
+
+		for(int i=0;i<N;i++){
+			if(works[i].finish==-1&&works[i].U<=F){
+				money-=works[i].X*(works[i].finish-works[i].U);
+			}
+		}
+		printf("Case %d: %d",i,money);
 	}
 }
