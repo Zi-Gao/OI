dd553c8528bf36db201670404547a4ec646a8756
Zi-Gao <gaogaohahaa@gmail.com>
2022-04-30 11:20:06 +0800

    .

--- Documents/Templates/stack.cpp
+++ Documents/Templates/stack.cpp
@@ -4,42 +4,51 @@
  */
 #include <cstdio>
 #include <cstring>
-#include <set>
-using namespace std;
-#define int long long
 //#define file
-#define stack_type int
-inline int read();
 
-const int maxstack = 100100;
-struct stack{
+//**********
+//#define STL_stack
+
+#ifdef STL_stack
+#include <stack>
+#endif
+
+#ifndef STL_stack
+#define stack_type int
+const int maxstack = 100000;
+struct stack
+{
 	stack_type _null,s[maxstack];
 	int s_top;
 
 	stack(){
 		s_top=0;
-		memset(s,0,sizeof(s));		
-		_null.second=-1;
-		_null.first=0;
+		memset(s,0,sizeof(s));
 		return;
 	}
-	
+
 	void push(stack_type in){
 		if(s_top < maxstack){
 			s[s_top] = in;
 			++s_top;
-		} 
+		}
 		return;
 	}
-	
+
 	void pop(){
 		if(s_top > 0){
-			s[s_top-1]=_null;
 			s_top--;
 		}
 		return;
 	}
-	
+
+	void pop(int in){
+		if(s_top >= in){
+			s_top-=in;
+		}
+		return;
+	}
+
 	stack_type top(){
 		if(s_top>0){
 			return s[s_top-1];
@@ -47,7 +56,11 @@ struct stack{
 			return _null;
 		}
 	}
-	
+
+	void clear(){
+		s_top=0;
+	}
+
 	bool empty(){
 		if(s_top==0){
 			return true;
@@ -55,22 +68,42 @@ struct stack{
 			return false;
 		}
 	}
-	
+
 	int size(){
 		return s_top;
 	}
-	
-	void clear(){
-		s_top=0;
+
+	stack_type read(int in){
+		if(s_top > in){
+			return s[in];
+		}else{
+			return _null;
+		}
+	}
+
+	void print(){
+		printf("size :%d\n", size());
+		printf("empty:%d\n\n", size());
 	}
 };
-signed main(){
+#endif
+//**********
+using namespace std;
+
+inline int read();
+
+int main(){
 	#ifdef file
-	freopen("rain.in", "r", stdin);
-	freopen("rain.out", "w", stdout);
+	freopen("name.in", "r", stdin);
+	freopen("name.out", "w", stdout);
 	#endif
 
-	
+	stack s;
+
+	s.push(1);
+	s.push(2);
+
+	printf("%d\n", s.read(1));
 
 	#ifdef file
 	fclose(stdin);
